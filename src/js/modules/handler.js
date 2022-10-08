c81051cfdc92a27b7f13b2dd6aee42bf2ffa188c
G_Dychala <g.dychala@gmail.com>
2022-10-08 22:58:04 +0200

    added localeStorage to Project

--- src/js/modules/handler.js
+++ src/js/modules/handler.js
@@ -1,5 +1,7 @@
 
-let counter=0;
+if(localStorage.counter)
+ {var counter = parseInt(localStorage.counter)}
+ else{var counter=0};
 
 
 export let handler=() => {
@@ -9,16 +11,16 @@ export let handler=() => {
   const pop_up=document.querySelector('.popup');
   const module=document.querySelector('.module');
   const over_lay=document.querySelector('.overlay');
+  const hide=()=>{pop_up.style.display="none"; over_lay.style.display="none"};
   
-  
-
   counter=counter+1;
   count.innerHTML=` ${counter}`;
   pop_up.style.display= "inline" ;
   over_lay.style.display="block";
   counter>5?reset.style.display="inline":null;
-  reset.addEventListener('click', ()=>{counter=0; count.innerHTML=` ${counter}`; reset.style.display="none"});
-  close.addEventListener('click', ()=>{pop_up.style.display="none"; over_lay.style.display="none"});
+  reset.addEventListener('click', ()=>{counter=0; count.innerHTML=` ${counter}`; reset.style.display="none", localStorage.setItem('counter', 0)});
+  close.addEventListener('click', hide);
+  over_lay.addEventListener('click', hide);
+  localStorage.setItem('counter', counter);
   }
-  
   
